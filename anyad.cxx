f4b6874c736833cafe17aea64989232933b9eb64
szilidaniel <34722259+szilidaniel@users.noreply.github.com>
2018-05-14 16:52:22 +0200

    Majd nem jó
    Csak 2 helyen rossz már

--- anyad.cxx
+++ anyad.cxx
@@ -10,18 +10,24 @@ double AtlagFrissit(double eddigi, double uj, int db){
 
 int main(){
 	bool jo = true;
+	bool szame = true;
 	double szam, atlag;
 	int db;
 	atlag=0;
 	db=0;
 	do{
-		cin >> szam;
-		jo = ((szam > 5) && (szam < 400));
-		if (jo){
-			atlag = AtlagFrissit(atlag, szam, db);
-			db++;
-		}
-	}while (jo);
+			cin >> szam;
+			szame = szam > 0;
+		if(szame){
+			jo = ((szam > 5) && (szam < 400));
+			if (jo){
+				atlag = AtlagFrissit(atlag, szam, db);
+				db++;
+			}else{
+			cout << "Nem mengfelelo tartomany! Az eddigi atlag: ";
+			}
+		}else cout << "nem szam";
+	}while (szam != '\n');
 	cout << atlag;
-	
+	return 0;
 }
