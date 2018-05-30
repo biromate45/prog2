a63c1304c674460022d8abc99a7c8888c99cacc7
szilidaniel <34722259+szilidaniel@users.noreply.github.com>
2018-05-31 00:56:40 +0200

    Revert "vizsga"
    This reverts commit 18b2c98754370844f226fb7fb122db39325f9213.

--- anyad.cxx
+++ anyad.cxx
@@ -27,7 +27,7 @@ int main(){
 			cout << "Nem mengfelelo tartomany! Az eddigi atlag: ";
 			}
 		}else cout << "nem szam";
-	}while (getline(cin, szam) && szam[0.00] != '\0')
-	;cout << atlag;
+	}while (szam != '\n');
+	cout << atlag;
 	return 0;
 }
